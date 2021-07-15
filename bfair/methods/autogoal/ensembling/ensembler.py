15d36680761bab1f2dc185bb6bfb0f9dc78df02f
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2021-07-15 15:54:40 +0000

    Implement `build_ensembler`

--- bfair/methods/autogoal/ensembling/ensembler.py
+++ bfair/methods/autogoal/ensembling/ensembler.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
 
+from autogoal.kb import MatrixCategorical, Supervised, VectorCategorical
+from autogoal.ml import AutoML
 from autogoal.sampling import Sampler
 from autogoal.search import PESearch
+from bfair.methods.voting import MLVotingClassifier, VotingClassifier
 from bfair.utils import ClassifierWrapper
 
 from .sampling import LogSampler, SampleModel
@@ -9,12 +12,28 @@ from .sampling import LogSampler, SampleModel
 
 class AutoGoalEnsembler:
     def __init__(
-        self, *, score_metric, errors="warn", allow_duplicates=False, **search_kwargs
+        self,
+        *,
+        score_metric,
+        errors="warn",
+        allow_duplicates=False,
+        include_filter=".*",
+        exclude_filter=None,
+        registry=None,
+        **search_kwargs,
     ):
         self.score_metric = score_metric
         self.search_kwargs = search_kwargs
         self.search_kwargs["errors"] = errors
         self.search_kwargs["allow_duplicates"] = allow_duplicates
+
+        self._pipeline_space = AutoML(
+            input=(MatrixCategorical, Supervised[VectorCategorical]),
+            output=VectorCategorical,
+            include_filter=include_filter,
+            exclude_filter=exclude_filter,
+            registry=registry,
+        ).make_pipeline_builder()
 
     def __call__(
         self,
@@ -60,8 +79,33 @@ class AutoGoalEnsembler:
         return best, best_fn
 
     def _build_generator_and_fn(self, X, y, X_test, y_test, classifiers, scores):
+        named_classifiers = {str(c): c for c in classifiers}
+        ensembler_types = ["voting", "learning"]
+
+        model_generator = self._pipeline_space
+
         def build_ensembler(sampler: LogSampler):
-            return ...  # TODO: use sampler to build ensembler
+            n_classifiers = sampler.discrete(
+                min=2,
+                max=len(classifiers),
+                handle="n_classifiers",
+            )
+            names = sampler.multichoice(
+                options=named_classifiers,
+                k=n_classifiers,
+                handle="selected_classifiers",
+            )
+            selected_classifiers = [named_classifiers[n] for n in names]
+
+            ensembler_type = sampler.choice(ensembler_types, handle="ensembler_type")
+            if ensembler_type == "voting":
+                ensembler = VotingClassifier(selected_classifiers)
+            elif ensembler_type == "learning":
+                pipeline = model_generator(sampler)
+                ensembler = MLVotingClassifier(
+                    selected_classifiers, model_init=lambda: ClassifierWrapper(pipeline)
+                )
+            return ensembler
 
         def generator(sampler: Sampler):
             sampler = LogSampler(sampler)
