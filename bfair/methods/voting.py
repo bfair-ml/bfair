f26d1546cd6c0e12f7f770e036440d0cd34031f2
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2021-07-15 15:54:00 +0000

    Implement `MLVotingClassifier`

--- bfair/methods/voting.py
+++ bfair/methods/voting.py
@@ -11,3 +11,21 @@ class VotingClassifier:
         stack = np.column_stack(predictions)
         most_commons = [Counter(sample).most_common(1)[0][0] for sample in stack]
         return np.asarray(most_commons)
+
+
+class MLVotingClassifier:
+    def __self__(self, estimators, model_init):
+        self.estimators = estimators
+        self.model = model_init()
+
+    def _build_input(self, X):
+        predictions = [model.predict(X) for model in self.estimators]
+        return np.column_stack(predictions)
+
+    def fit(self, X, y):
+        _input = self._build_input(X)
+        self.model.fit(_input, y)
+
+    def predict(self, X):
+        _input = self._build_input(X)
+        return self.model.predict(_input)
