791c3427abebc9844ff5a4f32f281bb09a922d65
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2021-07-29 13:03:17 +0000

    Update voting classifiers

--- bfair/methods/voting.py
+++ bfair/methods/voting.py
@@ -6,26 +6,32 @@ class VotingClassifier:
     def __init__(self, estimators):
         self.estimators = estimators
 
-    def predict(self, X):
-        predictions = [model.predict(X) for model in self.estimators]
-        stack = np.column_stack(predictions)
-        most_commons = [Counter(sample).most_common(1)[0][0] for sample in stack]
-        return np.asarray(most_commons)
-
-
-class MLVotingClassifier:
-    def __self__(self, estimators, model_init):
-        self.estimators = estimators
-        self.model = model_init()
-
-    def _build_input(self, X):
-        predictions = [model.predict(X) for model in self.estimators]
-        return np.column_stack(predictions)
+    def fit_estimators(self, X, y):
+        for estimator in self.estimators:
+            estimator.fit(X, y)
 
     def fit(self, X, y):
-        _input = self._build_input(X)
-        self.model.fit(_input, y)
+        pass
 
     def predict(self, X):
-        _input = self._build_input(X)
-        return self.model.predict(_input)
+        predictions = self._stack_predictions(self, X)
+        most_commons = [Counter(sample).most_common(1)[0][0] for sample in predictions]
+        return np.asarray(most_commons)
+
+    def _stack_predictions(self, X):
+        predictions = [model.predict(X) for model in self.estimators]
+        return np.column_stack(predictions)
+        
+
+class MLVotingClassifier(VotingClassifier):
+    def __self__(self, estimators, model_init):
+        super().__init__(estimators)
+        self.model = model_init()
+
+    def fit(self, X, y):
+        predictions = self._stack_predictions(X)
+        self.model.fit(predictions, y)
+
+    def predict(self, X):
+        predictions = self._stack_predictions(X)
+        return self.model.predict(predictions)
