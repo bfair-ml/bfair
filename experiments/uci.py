377af00b2a60f465e8640bca7f48998c0126da06
jpconsuegra <jpconsuegra93@gmail.com>
2022-05-19 14:35:48 +0000

    Add `succeeds_in_training_and_testing` to `experiments.uci`

--- experiments/uci.py
+++ experiments/uci.py
@@ -11,6 +11,7 @@ from autogoal.datasets import (
     yeast,
 )
 from autogoal.kb import MatrixContinuousDense
+from bfair.utils.autogoal import succeeds_in_training_and_testing
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
@@ -27,32 +28,44 @@ valid_datasets = [
 ]
 
 
-def load_dataset(name, max_examples=None):
+def get_dataset_and_loader(name):
     data = getattr(datasets, name).load()
+
     if len(data) == 4:
         X_train, y_train, X_test, y_test = data
     else:
         X, y = data
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    return (
-        X_train[:max_examples],
-        y_train[:max_examples],
-        X_test[:max_examples],
-        y_test[:max_examples],
-    )
+
+    def load_dataset(max_examples=None):
+        if max_examples is None:
+            return X_train, y_train, X_test, y_test
+        else:
+            return (
+                X_train[:max_examples],
+                y_train[:max_examples],
+                X_test[:max_examples],
+                y_test[:max_examples],
+            )
+
+    return (X_train, X_test, y_train, y_test), load_dataset
 
 
 def main():
     args = setup()
     selected_datasets = [args.title] if args.title in valid_datasets else valid_datasets
     for name in selected_datasets:
+        (X_train, X_test, y_train, _), load_dataset = get_dataset_and_loader(name)
         run(
-            load_dataset=partial(load_dataset, name=name),
+            load_dataset=load_dataset,
             input_type=MatrixContinuousDense,
             score_metric=accuracy_score,
             maximize=True,
             args=args,
             title=name,
+            diversifier_run_kwargs=dict(
+                constraint=succeeds_in_training_and_testing(X_train, y_train, X_test)
+            ),
         )
 
 
