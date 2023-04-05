c7691d1ffbf480539f7afec3c84d9c999a154acc
jpconsuegra <jpconsuegra93@gmail.com>
2023-04-05 11:33:53 +0000

    Update `SensorHandler` interface

--- bfair/sensors/handler.py
+++ bfair/sensors/handler.py
@@ -8,14 +8,51 @@ class SensorHandler:
         self.sensors = sensors
         self.merge = merge if merge else UnionMerge()
 
+    def __call__(
+        self,
+        item,
+        attributes: List[str],
+        attr_cls: str,
+        *,
+        stype: SemanticType = None,
+        use_all_sensors_if_inference_fails=True,
+    ):
+        stype = self._try_to_infer(item, stype=stype)
+
+        if stype is not None:
+            return self.annotate(item, stype, attributes, attr_cls)
+        elif use_all_sensors_if_inference_fails:
+            return self.forward(item, self.sensors, attributes, attr_cls)
+        raise ValueError(
+            "Unable to infer semantic type. Either pass it explicitly using `stype=...` or set `use_all_sensors_if_inference_fails=True`"
+        )
+
     def annotate(self, data, stype: SemanticType, attributes: List[str], attr_cls: str):
         sensors = self.select(stype, attr_cls)
-        annotations = [sensor(data, attributes, attr_cls) for sensor in sensors]
-        final = self.merge(annotations)
+        final = self.forward(data, sensors, attributes, attr_cls)
         return final
 
     def select(self, stype: SemanticType, attr_cls: str) -> List[Sensor]:
         return [sensor for sensor in self.sensors if sensor.match(stype, attr_cls)]
+
+    def forward(
+        self,
+        data,
+        sensors: List[Sensor],
+        attributes: List[str],
+        attr_cls: str,
+    ):
+        annotations = [sensor(data, attributes, attr_cls) for sensor in sensors]
+        final = self.merge(annotations)
+        return final
+
+    def _try_to_infer(self, item, *, stype: SemanticType = True):
+        if stype is not None:
+            return stype
+        try:
+            return SemanticType.infer(item)
+        except ValueError:
+            return None
 
 
 class UnionMerge:
