a3fe5cb9553d0b7ba2603c466fe41a6d3ac8f0d6
jpconsuegra <jpconsuegra93@gmail.com>
2023-04-28 11:57:29 +0000

    Add `MDGender.load_dataset` to `bfair.datasets`

--- bfair/datasets/__init__.py
+++ bfair/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .adult import load_dataset as load_adult
 from .german import load_dataset as load_german
 from .reviews import load_dataset as load_review
+from .mdgender import load_dataset as load_mdgender
