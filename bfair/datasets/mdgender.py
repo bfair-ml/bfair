c6a5a1b1af890a74f3cc5a9621d801f3691d2738
jpconsuegra <jpconsuegra93@gmail.com>
2023-05-04 14:52:31 +0000

    Add `just_certain` to `MDGender` dataset

--- bfair/datasets/mdgender.py
+++ bfair/datasets/mdgender.py
@@ -6,6 +6,7 @@ import datasets as db
 TEXT_COLUMN = "Text"
 GENDER_COLUMN = "Gender"
 CONFIDENCE_COLUMN = "Confidence"
+TYPE_COLUMN = "Annotation Type"
 
 MALE_VALUE = "Male"
 FEMALE_VALUE = "Female"
@@ -16,6 +17,7 @@ _CLASS_COLUMN = "class_type"
 _CONFIDENCE_COLUMN = "confidence"
 
 _ABOUT_VALUE = 0
+_CERTAIN_VALUE = "certain"
 
 _GENDER_MAP = {
     0: FEMALE_VALUE,
@@ -33,7 +35,7 @@ def load_dataset(split_seed=None, **kwargs):
 
 class MDGender(Dataset):
     @classmethod
-    def load(cls, just_about=True, split_seed=None):
+    def load(cls, just_about=True, just_certain=True, split_seed=None):
         source = db.load_dataset("md_gender_bias", "new_data", split="train")
 
         df = pd.DataFrame.from_dict(source)
@@ -41,11 +43,15 @@ class MDGender(Dataset):
             # for other `class_types` there are sentences that mention X gender but it is not annotated because it is not speaker or recipient.
             df = df[df[_CLASS_COLUMN] == _ABOUT_VALUE]
 
+        if just_certain:
+            df = df[df[_CONFIDENCE_COLUMN] == _CERTAIN_VALUE]
+
         gender = df[_LABELS_COLUMN].apply(lambda x: [_GENDER_MAP[key] for key in x])
         data = pd.concat(
             [
                 df[_TEXT_COLUMN].rename(TEXT_COLUMN),
                 df[_CONFIDENCE_COLUMN].rename(CONFIDENCE_COLUMN),
+                df[_CLASS_COLUMN].rename(TYPE_COLUMN),
                 gender.rename(GENDER_COLUMN),
             ],
             axis=1,
