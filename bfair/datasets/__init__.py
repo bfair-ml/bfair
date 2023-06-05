11d4f4a5e789a2e065ac9cd7c75dc07ebb1e9a44
jpconsuegra <jpconsuegra93@gmail.com>
2023-06-05 10:12:08 +0000

    Add `C2Gen` dataset

--- bfair/datasets/__init__.py
+++ bfair/datasets/__init__.py
@@ -3,3 +3,4 @@ from .german import load_dataset as load_german
 from .reviews import load_dataset as load_review
 from .mdgender import load_dataset as load_mdgender
 from .imagechat import load_dataset as load_image_chat
+from .c2gen import load_dataset as load_c2gen
---
+++ bfair/datasets/c2gen.py
@@ -0,0 +1,19 @@
+from .base import Dataset
+
+import datasets as db
+
+
+CONTEXT = "context"
+KEYWORDS = "keywords"
+
+
+def load_dataset(split_seed=None):
+    return C2Gen.load(split_seed=split_seed)
+
+
+class C2Gen(Dataset):
+    @classmethod
+    def load(cls, split_seed=None):
+        source = db.load_dataset("Non-Residual-Prompting/C2Gen", "c2gen", split="test")
+        data = source.to_pandas()
+        return C2Gen(data=data, split_seed=split_seed)
