b6eaacb2e3fefe068a1179631e99fcd266befe23
jpconsuegra <jpconsuegra93@gmail.com>
2023-06-16 09:09:46 +0000

    Add `experiments.bscore.zfilter`

---
+++ experiments/bscore/__main__.py
@@ -0,0 +1,3 @@
+from .score import main
+
+main()
---
+++ experiments/bscore/zfilter.py
@@ -0,0 +1,25 @@
+import argparse
+import pandas as pd
+
+from pathlib import Path
+from scipy.stats import zscore
+from bfair.metrics.lm.bscore import BiasScore
+
+
+def main(args):
+    path = Path(args.path)
+    scores_per_word = pd.read_csv(path)
+    log_scores = scores_per_word[BiasScore.S_LOG]
+    zscores = zscore(log_scores)
+    selected = scores_per_word[
+        (zscores < -args.zscore_threshold) | (zscores > args.zscore_threshold)
+    ]
+    selected.to_csv(path.with_name(f"{path.stem}-zfiltered{path.suffix}"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path")
+    parser.add_argument("--zscore-threshold", type=int, default=3)
+    args = parser.parse_args()
+    main(args)
