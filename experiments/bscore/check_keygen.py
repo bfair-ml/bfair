37509b7717277e4f3e63ccf6204d3e71cf488404
jpconsuegra <jpconsuegra93@gmail.com>
2023-07-17 11:04:37 +0000

    Add stemming to `check_keygen`

--- experiments/bscore/check_keygen.py
+++ experiments/bscore/check_keygen.py
@@ -2,7 +2,9 @@ import argparse
 import pandas as pd
 
 from pathlib import Path
+from functools import lru_cache
 from statistics import mean, StatisticsError
+from nltk.stem import SnowballStemmer
 from bfair.datasets.commongen import load_dataset as load_commongen, CONCEPTS
 from bfair.datasets.c2gen import load_dataset as load_c2gen, KEYWORDS
 from bfair.metrics.lm.bscore import ALL_GENDER_PAIRS
@@ -18,9 +20,18 @@ def default_mean(x, default=0):
         return default
 
 
+def get_stemmer_with_cache(args):
+    if not args.fuzzy_match:
+        return None
+    stemmer = SnowballStemmer(args.language)
+    return lru_cache(maxsize=None)(stemmer.stem)
+
+
 def main(args):
     path = Path(args.path)
     scores_per_word = pd.read_csv(path, index_col="words")
+
+    get_stem = get_stemmer_with_cache(args)
 
     if args.dataset == COMMON_GEN:
         dataset = load_commongen().all_data
@@ -35,7 +46,11 @@ def main(args):
         raise ValueError(args.dataset)
 
     in_keygens = scores_per_word.index.to_series().apply(
-        lambda word: [list(words) for words in keygens if word in words]
+        lambda word: [
+            list(words)
+            for words in keygens
+            if check_if_contained(word, tuple(words), get_stem)
+        ]
     )
 
     all_gender_words = {w for pair in ALL_GENDER_PAIRS for w in pair}
@@ -54,6 +69,15 @@ def main(args):
     print("- Ratio of gendered keygens:", default_mean(v > 0 for v in gendered_keygens))
 
 
+@lru_cache(maxsize=None)
+def check_if_contained(word, words, get_stem=None):
+    if get_stem is None:
+        return word in words
+
+    word = get_stem(word)
+    return any(word == get_stem(w) for w in words)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--path")
@@ -61,5 +85,8 @@ if __name__ == "__main__":
         "--dataset",
         help=f"Valid options: {[COMMON_GEN, C2GEN, '<PATH>']}",
     )
+    parser.add_argument("--fuzzy-match", action="store_true")
+    parser.add_argument("--language", default="english")
+
     args = parser.parse_args()
     main(args)
