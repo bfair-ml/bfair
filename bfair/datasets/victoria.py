dfaf63e9416da5271b5bd923dff5fcd5b2bbbadc
jpconsuegra <jpconsuegra93@gmail.com>
2024-09-05 10:32:21 +0000

    Update Victoria dataset

--- bfair/datasets/victoria.py
+++ bfair/datasets/victoria.py
@@ -14,9 +14,15 @@ def load_dataset(**kargs):
 
 class Victoria(Dataset):
     @classmethod
-    def load(cls, model, path="datasets/victoria", split_seed = None, stratify_by = None):
+    def load(
+        cls,
+        model,
+        path="datasets/victoria/LLMs",
+        split_seed=None,
+        stratify_by=None,
+    ):
         path = Path(path)
-        data_path = path / "02_Respuestas" / f"{model}.csv"
+        data_path = path / f"{model}.csv"
         data = pd.read_csv(data_path, sep=";")
         return Victoria(data=data, split_seed=split_seed, stratify_by=stratify_by)
 
