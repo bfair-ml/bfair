df33bb4049102e64d789bc682eab8217a5d52b43
jpconsuegra <jpconsuegra93@gmail.com>
2024-11-06 08:59:25 +0000

    Implement `experiments.sensors.annotate`

--- experiments/sensors/annotate.py
+++ experiments/sensors/annotate.py
@@ -4,7 +4,6 @@ import datasets as db
 
 from tqdm import tqdm
 from pathlib import Path
-from functools import partial
 
 from bfair.sensors import (
     P_GENDER,
@@ -13,14 +12,10 @@ from bfair.sensors import (
     CoreferenceNERSensor,
     DBPediaSensor,
     NameGenderSensor,
+    StaticWordsSensor,
 )
 from bfair.sensors.optimization import (
     load,
-    compute_errors,
-    compute_scores,
-    compute_multiclass_scores,
-    compute_multiclass_errors,
-    attributes_to_class,
 )
 from bfair.sensors.mocks import FixValueSensor, RandomValueSensor
 from bfair.datasets import (
@@ -29,41 +24,38 @@ from bfair.datasets import (
     load_image_chat,
     load_funpedia,
     load_toxicity,
+    load_villanos,
 )
 from bfair.datasets.reviews import (
     REVIEW_COLUMN as TEXT_COLUMN_REVIEW,
-    GENDER_COLUMN as GENDER_COLUMN_REVIEW,
     GENDER_VALUES as GENDER_VALUES_REVIEW,
-    SENTIMENT_COLUMN as TARGET_COLUMN_REVIEW,
 )
 from bfair.datasets.mdgender import (
     TEXT_COLUMN as TEXT_COLUMN_MDGENDER,
-    GENDER_COLUMN as GENDER_COLUMN_MDGENDER,
     GENDER_VALUES as GENDER_VALUES_MDGENDER,
 )
 from bfair.datasets.imagechat import (
     TEXT_COLUMN as TEXT_COLUMN_IMAGECHAT,
-    GENDER_COLUMN as GENDER_COLUMN_IMAGECHAT,
     GENDER_VALUES as GENDER_VALUES_IMAGECHAT,
 )
 from bfair.datasets.funpedia import (
     TEXT_COLUMN as TEXT_COLUMN_FUNPEDIA,
-    GENDER_COLUMN as GENDER_COLUMN_FUNPEDIA,
     GENDER_VALUES as GENDER_VALUES_FUNPEDIA,
-    NEUTRAL_VALUE as NEUTRAL_VALUE_FUNPEDIA,
 )
 from bfair.datasets.toxicity import (
     TEXT_COLUMN as TEXT_COLUMN_TOXICITY,
-    GENDER_COLUMN as GENDER_COLUMN_TOXICITY,
     GENDER_VALUES as GENDER_VALUES_TOXICITY,
 )
-from bfair.metrics import exploded_statistical_parity
+from bfair.datasets.villanos import (
+    TEXT_COLUMN as TEXT_COLUMN_VILLANOS,
+)
 
 DB_REVIEWS = "reviews"
 DB_MDGENDER = "mdgender"
 DB_IMAGECHAT = "imagechat"
 DB_FUNPEDIA = "funpedia"
 DB_TOXICITY = "toxicity"
+DB_VILLANOS = "villanos"
 
 
 def main():
@@ -72,6 +64,18 @@ def main():
     parser.add_argument("--config", action="append", required=True)
     parser.add_argument("--eval-all", action="store_true")
     parser.add_argument("--dump-path", default=None)
+    parser.add_argument(
+        "--datasets",
+        action="append",
+        choices=[
+            DB_REVIEWS,
+            DB_MDGENDER,
+            DB_IMAGECHAT,
+            DB_FUNPEDIA,
+            DB_TOXICITY,
+            DB_VILLANOS,
+        ],
+    )
     args = parser.parse_args()
     eval_all = args.eval_all
     dump_path = Path(args.dump_path) if args.dump_path is not None else None
@@ -113,13 +117,26 @@ def main():
             else:
                 print(f"Invalid handler configuration. {config_str}")
                 exit()
-        elif config_str == "defaults":
+        elif config_str.startswith("defaults"):
+            _, *params = config_str.split("|")
+            kwargs = {
+                name: value for param in params for name, value in (param.split(":"),)
+            }
+
+            def select(*arguments):
+                return {
+                    name: value for name, value in kwargs.items() if name in arguments
+                }
+
             handler = SensorHandler(
                 sensors=[
-                    EmbeddingBasedSensor.build_default_in_plain_mode(),
-                    CoreferenceNERSensor.build(),
-                    # DBPediaSensor.build(),
-                    NameGenderSensor.build(),
+                    EmbeddingBasedSensor.build_default_in_plain_mode(
+                        **select("language", "source")
+                    ),
+                    CoreferenceNERSensor.build(**select("language")),
+                    # DBPediaSensor.build(**select("language")),
+                    NameGenderSensor.build(**select("language")),
+                    StaticWordsSensor.build(**select("language")),
                 ],
             )
         else:
@@ -140,114 +157,80 @@ def main():
 
         handlers.append((config_str, handler))
 
-    def multilabel_errors(true_labels, predictions, gender_values):
-        errors = compute_errors(true_labels, predictions, gender_values)
-        print(errors)
-        scores = compute_scores(errors)
-        print(scores)
-
-    def multiclass_error(true_classes, predictions, neutral_value):
-        errors = compute_multiclass_errors(
-            true_classes,
-            predictions,
-            partial(attributes_to_class, neutral=neutral_value),
-        )
-        print(errors)
-        scores = compute_multiclass_scores(errors)
-        print(scores)
-
     # - Load DATASETS ---
-    datasets = [
+    all_datasets = [
         (
             f"{DB_REVIEWS} (complete)",
             lambda: load_review().data,
             TEXT_COLUMN_REVIEW,
-            GENDER_COLUMN_REVIEW,
             GENDER_VALUES_REVIEW,
-            TARGET_COLUMN_REVIEW,
-            None,
         ),
         (
             f"{DB_REVIEWS} (training)",
             lambda: load_review(split_seed=0).data,
             TEXT_COLUMN_REVIEW,
-            GENDER_COLUMN_REVIEW,
             GENDER_VALUES_REVIEW,
-            TARGET_COLUMN_REVIEW,
-            None,
         ),
         (
             f"{DB_REVIEWS} (testing)",
             lambda: load_review(split_seed=0).test,
             TEXT_COLUMN_REVIEW,
-            GENDER_COLUMN_REVIEW,
             GENDER_VALUES_REVIEW,
-            TARGET_COLUMN_REVIEW,
-            None,
         ),
         (
             DB_MDGENDER,
             lambda: load_mdgender().data,
             TEXT_COLUMN_MDGENDER,
-            GENDER_COLUMN_MDGENDER,
             GENDER_VALUES_MDGENDER,
-            None,
-            None,
         ),
         (
             f"{DB_IMAGECHAT} (training)",
             lambda: load_image_chat().data,
             TEXT_COLUMN_IMAGECHAT,
-            GENDER_COLUMN_IMAGECHAT,
             GENDER_VALUES_IMAGECHAT,
-            None,
-            None,
         ),
         (
             f"{DB_IMAGECHAT} (testing)",
             lambda: load_image_chat().test,
             TEXT_COLUMN_IMAGECHAT,
-            GENDER_COLUMN_IMAGECHAT,
             GENDER_VALUES_IMAGECHAT,
-            None,
-            None,
         ),
         (
             f"{DB_TOXICITY} (training @ 0.0)",
             lambda: load_toxicity(threshold=0).data,
             TEXT_COLUMN_TOXICITY,
-            GENDER_COLUMN_TOXICITY,
             GENDER_VALUES_TOXICITY,
-            None,
-            None,
         ),
         (
             f"{DB_TOXICITY} (training @ 0.5)",
             lambda: load_toxicity(threshold=0.5).data,
             TEXT_COLUMN_TOXICITY,
-            GENDER_COLUMN_TOXICITY,
             GENDER_VALUES_TOXICITY,
-            None,
-            None,
         ),
         (
             f"{DB_FUNPEDIA} (testing)",
             lambda: load_funpedia().test,
             TEXT_COLUMN_FUNPEDIA,
-            GENDER_COLUMN_FUNPEDIA,
             GENDER_VALUES_FUNPEDIA,
-            None,
-            NEUTRAL_VALUE_FUNPEDIA,
         ),
         (
             f"{DB_FUNPEDIA} (all data)",
             lambda: load_funpedia().all_data,
             TEXT_COLUMN_FUNPEDIA,
-            GENDER_COLUMN_FUNPEDIA,
             GENDER_VALUES_FUNPEDIA,
-            None,
-            NEUTRAL_VALUE_FUNPEDIA,
         ),
+        (
+            f"{DB_VILLANOS} (all data)",
+            lambda: load_villanos().all_data,
+            TEXT_COLUMN_VILLANOS,
+            ["male", "female"],
+        ),
+    ]
+
+    datasets = [
+        ds
+        for ds in all_datasets
+        if not args.datasets or ds[0].split(" ")[0] in args.datasets
     ]
 
     db.logging.set_verbosity_error()
@@ -260,27 +243,18 @@ def main():
             dataset_name,
             load_dataset,
             text_column,
-            gender_column,
             gender_values,
-            target_column,
-            neutral_value,
         ) in tqdm(datasets, desc="Datasets"):
 
             print(f"\n# {dataset_name}")
             data = load_dataset()
 
             X = data[text_column]
-            y = data[gender_column]
 
             predictions = [
                 handler(text, gender_values, P_GENDER)
                 for text in tqdm(X, desc=f"Texts {dataset_name}")
             ]
-
-            if neutral_value is None:
-                multilabel_errors(y, predictions, gender_values)
-            else:
-                multiclass_error(y, predictions, neutral_value)
 
             if dump_path is not None:
                 all_predictions = [("Hander", predictions)]
@@ -293,7 +267,6 @@ def main():
                 results = pd.concat(
                     (
                         X,
-                        y.str.join(" & ") if neutral_value is None else y,
                         *[
                             pd.Series(pred, name=name, index=X.index)
                             .apply(sorted)
@@ -304,32 +277,6 @@ def main():
                     axis=1,
                 )
                 results.to_csv((dump_path / dataset_name).with_suffix(".csv"))
-
-            if target_column is None:
-                continue
-
-            fairness = exploded_statistical_parity(
-                data=data,
-                protected_attributes=gender_column,
-                target_attribute=target_column,
-                target_predictions=None,
-                positive_target="positive",
-                return_probs=True,
-            )
-            print("## True fairness:", fairness)
-
-            auto_annotated = data.copy()
-            auto_annotated[gender_column] = [list(x) for x in predictions]
-
-            fairness = exploded_statistical_parity(
-                data=auto_annotated,
-                protected_attributes=gender_column,
-                target_attribute=target_column,
-                target_predictions=None,
-                positive_target="positive",
-                return_probs=True,
-            )
-            print("## Estimated fairness:", fairness)
 
 
 if __name__ == "__main__":
