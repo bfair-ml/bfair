6293e447bce10b3971c1157aa98f70505488f2e0
jpconsuegra <jpconsuegra93@gmail.com>
2026-01-14 16:16:07 +0000

    Enhance run-all.sh to include output file checks and improve logging format

--- runs/runs-rhopa64/run-all.sh
+++ runs/runs-rhopa64/run-all.sh
@@ -15,12 +15,20 @@ for model in "${models[@]}"; do
       fi
       for subtheme_id in "${subtheme_ids[@]}"; do
         language="${languages[$i]}"
+
+        # IFS='/' read -r a b c model language theme_id subtheme_id <<< ""./runs/runs-rhopa64/salamandra-2b-instruct_databias_generated/english/22.0/22.3"" # Set custom values for testing
+
         path_prefix="runs/runs-rhopa64/${model}/${language}/${theme_id}/${subtheme_id}"
 
-        echo "Model: ${model}, Language: ${language}, Theme ID: ${theme_id}, Subtheme ID: ${subtheme_id}"
+        echo "============= Model: ${model}, Language: ${language}, Theme ID: ${theme_id}, Subtheme ID: ${subtheme_id} ============="
 
         mkdir -p "${path_prefix}"
         python -m experiments.bscore --context continuous --dataset "rhopa64|language:${language}|model:${model}|theme_id:${theme_id}|subtheme_id:${subtheme_id}" --export-csv "${path_prefix}/aggregation.csv" --export-scores "${path_prefix}/.scores.json" --use-root yes --lower-proper-nouns yes --semantic-check no --split-endings no   --inspect-professions no --use-morph no >> "${path_prefix}/.log.txt"
+        
+        if [[ $(cat "${path_prefix}/aggregation.csv") == "words,pos,count_disparity,log_score" ]]; then
+          echo "Skipping further processing as the output file contains only the header."
+          continue
+        fi
         python -m experiments.bscore.zfilter --path "${path_prefix}/aggregation.csv" --zscore-threshold 2
         python -m experiments.bscore.filterby --path "${path_prefix}/aggregation-z2filtered.csv" --pos ADJ
         python -m experiments.bscore.filterby --path "${path_prefix}/aggregation-z2filtered.csv" --pos VERB
